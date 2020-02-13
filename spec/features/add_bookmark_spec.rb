06055022d6b5a778771fd14e6905014f001d1319
Peter <pc.dean@icloud.com>
2020-02-13 15:10:10 +0000

    title and url user input for new bookmarks

--- spec/features/add_bookmark_spec.rb
+++ spec/features/add_bookmark_spec.rb
@@ -1,8 +1,9 @@
 feature 'add bookmark' do
   scenario 'it adds a bookmark' do
     visit('/')
-    fill_in 'new_bookmark', with: 'http:www.youtube.com'
+    fill_in 'title', with: 'youtube'
+    fill_in 'url', with: 'http:www.youtube.com'
     click_button "Add Bookmark"
-    expect(page).to have_content('http:www.youtube.com')
+    expect(page).to have_link('youtube', href: 'http:www.youtube.com')
   end
 end
