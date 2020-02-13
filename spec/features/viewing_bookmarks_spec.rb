2e116345fdfa8ae82f5b4296ef6941ff13900c02
Peter <pc.dean@icloud.com>
2020-02-13 10:48:27 +0000

    refactored test database setup

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,12 +1,5 @@
 feature 'viewing bookmarks' do
 
-  before(:each) do
-    connection = Bookmark.set_env
-    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
-  end
-
   scenario 'it shows me all the bookmarks' do
     visit('/bookmark_list')
     expect(page).to have_content "http://www.makersacademy.com"
