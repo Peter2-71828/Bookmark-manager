34866973db93fa12ac4dd883f1a2a5fda3eaea0d
Peter <pc.dean@icloud.com>
2020-02-14 10:33:58 +0000

    link works valid adress check not yet implemented

--- spec/features/add_bookmark_spec.rb
+++ spec/features/add_bookmark_spec.rb
@@ -2,8 +2,8 @@ feature 'add bookmark' do
   scenario 'it adds a bookmark' do
     visit('/')
     fill_in 'title', with: 'youtube'
-    fill_in 'url', with: 'http:www.youtube.com'
+    fill_in 'url', with: 'http://www.youtube.com'
     click_button "Add Bookmark"
-    expect(page).to have_link('youtube', href: 'http:www.youtube.com')
+    expect(page).to have_link('youtube', href: 'http://www.youtube.com')
   end
 end
