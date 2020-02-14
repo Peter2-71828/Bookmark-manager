1ee72be3200e1f29d2a11f9556f0652a869a9727
Peter <pc.dean@icloud.com>
2020-02-14 10:36:35 +0000

    updated test to check for book title

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -2,9 +2,9 @@ feature 'viewing bookmarks' do
 
   scenario 'it shows me all the bookmarks' do
     visit('/bookmark_list')
-    expect(page).to have_content "http://www.makersacademy.com"
-    expect(page).to have_content "http://www.destroyallsoftware.com"
-    expect(page).to have_content "http://www.google.com"
+    expect(page).to have_content "makers"
+    expect(page).to have_content "destroy all software"
+    expect(page).to have_content "google"
   end
 
 end
--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -2,7 +2,7 @@ require 'pg'
 
 def setup_test_database
   connection = Environment.connection
-  connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-  connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
-  connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
+  connection.exec("INSERT INTO bookmarks (title, url) VALUES ('makers', 'http://www.makersacademy.com');")
+  connection.exec("INSERT INTO bookmarks (title, url) VALUES ('destroy all software', 'http://www.destroyallsoftware.com');")
+  connection.exec("INSERT INTO bookmarks (title, url) VALUES ('google', 'http://www.google.com');")
 end
